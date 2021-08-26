2613a0f5225d00faa6d9c65a84449049e92ebaef
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-08-26 22:16:58 +0200

    1.5 Thread priority #1

--- src/main/java/com/multithreading/ThreadExample.java
+++ src/main/java/com/multithreading/ThreadExample.java
@@ -5,18 +5,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ThreadExample {
 
-
-    public static void main(String[] args) {
-
-        Thread thread = new Thread(() -> {
-            for (int i =0; i<20;i++){
-                log.info(" second thread loop: " + i);
-            }
-        });
-        thread.start();
-
-        for (int i =0; i<20;i++){
+    private static void mainThread() throws InterruptedException {
+        Thread.sleep(2000);
+        log.info("Starting loop in thread: " +Thread.currentThread().getName());
+        for (int i =0; i<50;i++){
             log.info(" first thread loop: " + i);
         }
+    }
+
+    private static void secondThread(){
+        log.info("Starting loop in thread: " +Thread.currentThread().getName());
+        for (int i =0; i<20;i++){
+            log.info(" second thread loop: " + i);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread(ThreadExample::secondThread);
+        thread.start();
+
+        elapsedTimeMainThread();
+
+
+        Thread thread1 = new Thread(()->
+                log.info("Starting another thread " + Thread.currentThread().getName())
+        );
+        thread1.start();
+        thread1.setPriority(Thread.MIN_PRIORITY);
+}
+
+    private static void elapsedTimeMainThread() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        mainThread();
+        long stop = System.currentTimeMillis();
+        log.info("Elapsed time: " + (stop-start));
     }
 }
